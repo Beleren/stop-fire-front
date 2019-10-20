ed6868fa564c890604db2ea024119337e57e4311
Leandro <leandroharuki@gmail.com>
2019-10-20 04:58:33 -0300

    Alteracao de component de home para map

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Home from "./scenes/Home";
+import Map from "./components/Map";
 
 function App() {
   return (
     <Router>
       <Switch>
         <Route path="/">
-          <Home />
+          <Map />
         </Route>
       </Switch>
     </Router>
